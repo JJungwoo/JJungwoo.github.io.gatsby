9090f217756c33f38f5fef3e3b6c0982f448d45b
JJungwoo <jungwoo5759@gmail.com>
2021-07-08 00:58:59 +0900

    init gatsby github.io

---
+++ config.js
@@ -0,0 +1,49 @@
+'use strict';
+
+module.exports = {
+  // url: 'https://lumen.netlify.com',
+  url: "https://github.com/JJungwoo",
+  pathPrefix: '/',
+  title: 'Blog by Jungwoo Jo',
+  // subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+  subtitle: '',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  // googleAnalyticsId: 'UA-73379983-2',
+  useKatex: false,
+  menu: [
+    {
+      label: 'Contents',
+      path: '/'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    }
+  ],
+  author: {
+    name: 'Jungwoo Jo',
+    photo: '/jungwoo.jpeg',
+    // bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    bio: 'hi',
+    contacts: {
+      email: 'jungwoo5759@gmail.com',
+      // facebook: '#',
+      // telegram: '#',
+      // twitter: '#',
+      github: 'JJungwoo',
+      // rss: '',
+      // vkontakte: '',
+      // linkedin: '#',
+      // instagram: '#',
+      // line: '',
+      // gitlab: '',
+      // weibo: '',
+      // codepen: '',
+      // youtube: '',
+      // soundcloud: '',
+      // medium: '',
+    }
+  }
+};
